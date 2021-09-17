ecb716120e1d4ba1f948343f4e29616782287c38
Serj Sidelnikov <serj2107@gmail.com>
2021-09-17 18:37:37 +0300

    WIP

---
+++ src/frontend/src/modules/profile/components/ProfileAddress.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sheet address-form">
+    <div class="address-form__header">
+      <b>Адрес №1. Тест</b>
+
+      <div class="address-form__edit">
+        <button type="button" class="icon">
+          <span class="visually-hidden">Изменить адрес</span>
+        </button>
+      </div>
+    </div>
+
+    <p>Невский пр., д. 22, оф. 46</p>
+    <small>Позвоните, пожалуйста, от проходной</small>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfileAddress",
+};
+</script>
