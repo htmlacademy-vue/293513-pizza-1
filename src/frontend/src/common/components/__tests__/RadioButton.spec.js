50ec4ae1fd37bde7b79383ddb4bc1bc374c99ca5
Serj <serj2107@gmail.com>
2021-11-21 11:07:35 +0300

    WIP

---
+++ src/frontend/src/common/components/__tests__/RadioButton.spec.js
@@ -0,0 +1,43 @@
+import { shallowMount } from "@vue/test-utils";
+import RadioButton from "@/common/components/RadioButton";
+
+describe("RadioButton", () => {
+  const slots = { default: "content" };
+  let props;
+
+  let wrapper;
+  const createComponent = (options) => {
+    wrapper = shallowMount(RadioButton, options);
+  };
+
+  beforeEach(() => {
+    props = {
+      name: "test",
+      value: "test",
+      checked: false,
+    };
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  it("Renders out the slot content", () => {
+    createComponent({ slots, propsData: props });
+    expect(wrapper.html()).toContain(slots.default);
+  });
+
+  it("Raises the change event on change", async () => {
+    createComponent({ slots, propsData: props });
+    const input = wrapper.find("input");
+    await input.trigger("change");
+    expect(wrapper.emitted("change")).toBeTruthy();
+  });
+
+  it("Changes state checked on change event", async () => {
+    createComponent({ slots, propsData: props });
+    const input = wrapper.find("input");
+    await input.trigger("change");
+    expect(input.element.checked).toBeTruthy();
+  });
+});
