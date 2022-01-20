bcd4f66230dabc7cc212c24a5d2225b4c2e34dd7
Serj Sidelnikov <serj.v.sidelnikov@gmail.com>
2022-01-20 19:55:08 +0300

    Fix

--- src/frontend/babel.config.js
+++ src/frontend/babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
-  plugins: ["require-context-hook"],
+  env: {
+    test: {
+      plugins: ["require-context-hook"],
+    },
+  },
 };
