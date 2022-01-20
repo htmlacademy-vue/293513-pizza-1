4037c1bc893d9c7ffabc2c43e919b994fb43c728
Serj Sidelnikov <serj.v.sidelnikov@gmail.com>
2022-01-20 14:51:52 +0300

    Fix tests

--- src/frontend/jest.config.js
+++ src/frontend/jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFiles: ["<rootDir>/.jest.register-context.js"],
 };
